c06da07b376be8b22352c0f72e8bd435e3b3f9de
Joseph Herrera <bjlherrera@gmail.com>
2021-11-24 22:14:12 +0800

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,14 @@
 #!/usr/local/bin/bash
-NAME=$1
+EXE_NAME=$1
 shift
-FILE=$@
-/usr/bin/clang++ -o $NAME -std=c++17 -Wall $FILE
+MAIN_NAME=$(basename -- "$1")
+MAIN_EXT="${MAIN_NAME##*.}"
+SRC=$@
+
+case $MAIN_EXT in
+  cc | cpp)
+    /usr/bin/clang++ -o $EXE_NAME -std=c++17 -Wall $SRC
+    ;;
+  *)
+    echo "Support for .$MAIN_EXT source code has not been implemented yet."
+esac
