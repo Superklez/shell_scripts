60dad7b847f0c3b4c523bd0e313cd700ca7170c4
Joseph Herrera <bjlherrera@gmail.com>
2022-01-08 14:26:02 +0800

    Revert back to clang/clang++

--- compile.sh
+++ compile.sh
@@ -7,10 +7,10 @@ SRC=$@
 
 case $MAIN_EXT in
   cc | cpp)
-    /usr/local/bin/g++-11 -o $EXE_NAME -std=c++17 -Wall $SRC
+    /usr/bin/clang++ -o $EXE_NAME -std=c++17 -Wall $SRC
     ;;
   c)
-    /usr/local/bin/gcc-11 -o $EXE_NAME -std=c17 -Wall $SRC
+    /usr/bin/clang -o $EXE_NAME -std=c17 -Wall $SRC
     ;;
   *)
     echo "Support for .$MAIN_EXT source code has not been implemented yet."
