28a2e0219d2d50b08eb1fac92c3c9618da762b3f
Joseph Herrera <bjlherrera@gmail.com>
2022-02-01 22:20:26 +0800

    Added option to create dynamic library
    The .dylib extension only works in macOS. For Windows, use the .dll extension. For Linux, use the .so extension.

--- make_library.sh
+++ make_library.sh
@@ -1,17 +1,18 @@
 #!/usr/local/bin/bash
 LIB="$1"
+LIB_EXT="${LIB##*.}"
+
 shift
 SRC=$@
-OBJ=()
 
-TEMP=$(basename -- "$1")
-EXT="${TEMP##*.}"
+TEMP="$1"
+SRC_EXT="${TEMP##*.}"
 
-if [ "$EXT" == "cc" ]; then
-  EXT="cpp"
+if [ "$SRC_EXT" == "cc" ]; then
+  SRC_EXT="cpp"
 fi
 
-LIB="$HOME/local/libraries/$EXT/$LIB"
+LIB="$HOME/local/libraries/$SRC_EXT/$LIB"
 
 if [ -e "$LIB" ]; then
     echo "ERROR: $LIB already exists."
@@ -25,17 +26,31 @@ function cleanup {
   return 0
 }
 
-for src in $SRC; do
-  echo "Compiling $src"
-  obj=${src%.*}.o
-  $HOME/bin/compile.sh $obj $src -c &> /dev/null ||
-    (echo "ERROR: Compilation failed."; cleanup ${OBJ[*]}; exit 2)
-  OBJ+=($obj)
-done
+if [ "$LIB_EXT" == "dylib" ]; then
+  # Dynamic library
+  echo "Creating dynamic library at $LIB"
+  $HOME/bin/compile.sh $LIB $SRC -fPIC -shared ||
+    (echo "ERROR: Failed creating dynamic library."; exit 2)
 
-echo "Creating archive at $LIB"
-/usr/bin/ar rcs $LIB ${OBJ[*]} && echo "Process complete." ||
-  echo "ERROR: Failed creating archive."
+elif [ "$LIB_EXT" == "a" ]; then
+  # Static library
+  OBJ=()
+  for src in $SRC; do
+    echo "Compiling $src"
+    obj=${src%.*}.o
+    $HOME/bin/compile.sh $obj $src -c &> /dev/null ||
+      (echo "ERROR: Compilation failed."; cleanup ${OBJ[*]}; exit 2)
+    OBJ+=($obj)
+  done
 
-cleanup ${OBJ[*]}
+  echo "Creating archive at $LIB"
+  /usr/bin/ar rcs $LIB ${OBJ[*]} && echo "Process complete." ||
+    (echo "ERROR: Failed creating archive."; exit 3)
+
+  cleanup ${OBJ[*]}
+
+else
+  echo "ERROR: Unsupported library extension."
+  exit 4
+fi
 exit 0
