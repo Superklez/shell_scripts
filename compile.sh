29daa1ae030d815ff16caf995ba52f489aa2d44b
Joseph Herrera <bjlherrera@gmail.com>
2022-10-01 11:48:28 +0800

    Switch to gcc and g++
    The compilers gcc-12 and g++-12 were downloaded using Homebrew. In my system, the default gcc and g++ are just the clang and clang++ compilers, respectively.

--- compile.sh
+++ compile.sh
@@ -7,12 +7,12 @@ SRC=$@
 
 case $MAIN_EXT in
   cc | cpp)
-    /usr/bin/clang++ -o $EXE_NAME -std=c++17 -Wall -I$HOME/local/includes/cpp \
-      -L$HOME/local/libraries/cpp $SRC
+    /usr/local/bin/g++-12 -o $EXE_NAME -std=c++17 -Wall \
+    -I$HOME/local/include/ $SRC -L$HOME/local/library/
     ;;
   c)
-    /usr/bin/clang -o $EXE_NAME -std=c89 -Wall -I$HOME/local/includes/c \
-      -L$HOME/local/libraries/c $SRC
+    /usr/local/bin/gcc-12 -o $EXE_NAME -std=c90 -Wall \
+    -I$HOME/local/include/ $SRC -L$HOME/local/library/
     ;;
   *)
     echo "Support for .$MAIN_EXT source code has not been implemented yet."
